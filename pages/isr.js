54797704de06e0200cdf2be582d26647cd22286c
hansol <hansssol5202@gmail.com>
2022-10-02 13:58:40 +0900

    ISR 방식

--- pages/isr.js
+++ pages/isr.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
-import Link from 'next/link'
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   console.log("i am server")
   return{
-    props:{time: new Date().toISOString()}
+    props:{time: new Date().toISOString()},
+    revalidate: 1,
   }
 }
 
-export default function Home({time}) {
+export default function SSG({ time }) {
   return (
     <div className="container">
       <Head>
@@ -20,9 +20,6 @@ export default function Home({time}) {
         <h1 className="title">
           {time}
         </h1>
-        <h1><Link href="/csr"><a>CSR로</a></Link></h1>
-        <h1><Link href="/ssg"><a>SSG로</a></Link></h1>
-        <h1><Link href="/isr"><a>ISR로</a></Link></h1>
       </main>
 
       <footer>
