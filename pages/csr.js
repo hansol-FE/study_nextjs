45ec0f2945b6e2d78bb21397ab48669d940a2353
hansol <hansssol5202@gmail.com>
2022-10-02 13:25:30 +0900

    CSR 방식의 데이터 fatch

--- pages/csr.js
+++ pages/csr.js
@@ -1,14 +1,15 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
-export async function getServerSideProps() {
-  console.log("i am server")
-  return{
-    props:{time: new Date().toISOString()}
-  }
-}
 
-export default function Home({time}) {
+export default function Home() {
+  const [time, setTime] = useState()
+
+  useEffect(()=>{
+    console.log("client")
+    setTime(new Date().toISOString())
+  },[])
   return (
     <div className="container">
       <Head>
@@ -20,7 +21,6 @@ export default function Home({time}) {
         <h1 className="title">
           {time}
         </h1>
-        <h1><Link href="/csr"><a>CSR로</a></Link></h1>
       </main>
 
       <footer>
