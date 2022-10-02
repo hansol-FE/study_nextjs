9eaf12727b0bebabe6bd9acc8819989a7bec65ed
hansol <hansssol5202@gmail.com>
2022-10-02 14:17:25 +0900

    SubLayout으로 감싸기

---
+++ components/SubLayout.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function SubLayout({children}) {
+  return (
+    <div>
+      <h1><Link href="/"><a>Home으로</a></Link></h1>
+      {children}
+    </div>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import Layout from "../components/Layout";
 
 export default function APP({Component, pageProps}){
-    return(
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    )
+    // return(
+    //     <Layout>
+    //         <Component {...pageProps} />
+    //     </Layout>
+    // )
+
+    const getLayout = Component.getLayout || ((page)=> <Layout>{page}</Layout>)
+
+    return getLayout(<Component {...pageProps} />)
 }
