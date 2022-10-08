dcab74bc04aa3ac7e6f6d74e7894610b8761dce7
hansol <dundunki@naver.com>
2022-10-08 12:46:20 +0900

    add user api

--- src/pages/[username]/[info].js
+++ src/pages/[username]/[info].js
@@ -1,13 +1,23 @@
 import Layout from 'components/Layout'
 import SubLayout from 'components/SubLayout'
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 export default function UsernameInfo() {
   const router = useRouter()
   const {username, info} = router.query
+  const [ name, setName ] = useState('?');
+
+  useEffect(()=>{
+    fetch('/api/user').then((res)=> res.json()).then((data)=>{
+      setName(data.name)
+    })
+  },[])
+
   return (
     <>
-      <h1 className="title">{username}~ {info}</h1>
+      <h1 className="title">router에서 받은 uername: {username}~ {info}</h1>
+      <h1 className="title">api로받은 username: {name}</h1>
     </>
   )
 }
