8d4c9d14e389e0bec1bf87ca94a5a4923d07c4ee
hansol <dundunki@naver.com>
2022-10-10 15:02:20 +0900

    ssg로 파일읽어오고 pre-rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,50 @@
-import Link from 'next/link'
+import Head from 'next/head';
+import Image from 'next/image';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export async function getServerSideProps() {
-  console.log('i am server')
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
   return {
-    props: { time: new Date().toISOString() },
-  }
+    props: {
+      allPostsData,
+    },
+  };
 }
 
-export default function Home({ time }) {
+
+export default function Home({allPostsData}) {
   return (
     <>
-       <h1>
-        Read this posts{' '}
-        <Link href="/posts/first-post">
-          <a>첫번째 글</a>
-        </Link>
-      </h1>
-      <h1 className="title">{time}</h1>
-      <h1>
-        <Link href="/csr">
-          <a>CSR로</a>
-        </Link>
-      </h1>
-      <h1>
-        <Link href="/ssg">
-          <a>SSG로</a>
-        </Link>
-      </h1>
-      <h1>
-        <Link href="/isr">
-          <a>ISR로</a>
-        </Link>
-      </h1>
-      <h1>
-        Read this posts{' '}
-        <Link href="/posts/second-post">
-          <a>두번째 글</a>
-        </Link>
-      </h1>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>안녕하세요.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <p>ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ</p>
+              글 제목: {title}
+              <br />
+              <br />
+              파일의 이름: {id}
+              <br />
+              날짜: {date}
+            </li>
+            
+          ))}
+        </ul>
+      </section>
     </>
-  )
+  );
 }
