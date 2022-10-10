818d15aab6ce01f44bb68def14c47bd0befafbbd
hansol <dundunki@naver.com>
2022-10-10 15:57:38 +0900

    remark로 html 읽어오기

---
+++ src/pages/posts/[id].js
@@ -0,0 +1,39 @@
+import Layout from "components/Layout";
+import Head from "next/head";
+import { getAllPostIds, getPostData } from 'lib/posts';
+
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <>
+        <Head>
+            <title>{postData.id}</title>
+        </Head>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </>
+  )
+}
--- src/pages/posts/first-post.js
+++
@@ -1,3 +0,0 @@
-export default function FirstPost() {
-    return <h1>첫 게시물</h1>
-}
